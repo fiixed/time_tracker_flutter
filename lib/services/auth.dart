5895f4e42f0165456e6f658db1c92aa3d165cd74
Andrew Bell <fiixed@gmail.com>
2022-01-13 21:31:36 -0500

    fb login working

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,17 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   User get currentUser;
   Future<User> signInAnonymously();
-  Future<void> signOut();
   Stream<User> authStateChanges();
   Future<User> signInWithGoogle();
+  Future<User> signInWithFacebook();
+  Future<void> signOut();
 }
 
 class Auth implements AuthBase {
   final _firebaseAuth = FirebaseAuth.instance;
   final googleSignIn = GoogleSignIn();
+  final fb = FacebookLogin();
 
   @override
   Stream<User> authStateChanges() => _firebaseAuth.authStateChanges();
@@ -53,8 +56,40 @@ class Auth implements AuthBase {
   }
 
   @override
+  Future<User> signInWithFacebook() async {
+    final response = await fb.logIn(permissions: [
+      FacebookPermission.publicProfile,
+      FacebookPermission.email,
+    ]);
+    switch (response.status) {
+      case FacebookLoginStatus.success:
+        final accessToken = response.accessToken;
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          FacebookAuthProvider.credential(accessToken.token),
+        );
+        return userCredential.user;
+        break;
+      case FacebookLoginStatus.cancel:
+        throw FirebaseAuthException(
+          code: 'ERROR_ABORTED_BY_USER',
+          message: 'Sign in aborted by user',
+        );
+        break;
+      case FacebookLoginStatus.error:
+        throw FirebaseAuthException(
+          code: 'ERROR_FACEBOOK_LOGIN_FAILED',
+          message: response.error.developerMessage,
+        );
+        break;
+      default:
+        throw UnimplementedError();
+    }
+  }
+
+  @override
   Future<void> signOut() async {
     await googleSignIn.signOut();
+    await fb.logOut();
     await _firebaseAuth.signOut();
   }
 }
